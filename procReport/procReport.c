898a2f3f4357801a8e175ce1804d0242cc16036a
Eviatar Goldschmidt <35406519+Evigold@users.noreply.github.com>
2019-03-10 00:27:45 -0800

    Add files via upload

---
+++ procReport/procReport.c
@@ -0,0 +1,15 @@
+#include<linux/module.h>
+
+int proc_init (void) {
+  printk(KERN_INFO "helloModule: kernel module initialized\n");
+  return 0;
+}
+
+void proc_cleanup(void) {
+  printk(KERN_INFO "helloModule: performing cleanup of module\n");
+}
+
+MODULE_LICENSE("GPL");
+module_init(proc_init);
+module_exit(proc_cleanup);
+
