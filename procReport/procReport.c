4476d29b30b3f1dcda7196a76927331e5ee588f9
evi <evigold@uw.edu>
2019-03-17 03:21:38 -0700

    comments added, still want to format output for things to be alligned

--- procReport/procReport.c
+++ procReport/procReport.c
@@ -6,7 +6,9 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
-// Struct to store info about each proccess
+/**
+ * Struct to store info about each proccess.
+ **/
 typedef struct proc_data {
   int proc_id;
   char *proc_name;
@@ -15,25 +17,35 @@ typedef struct proc_data {
   struct proc_data *next;
 } proc_data;
 
-// Struct to store global variables.
+/**
+ * Struct to store global variables.
+ **/
 typedef struct data_head {
   proc_data *head;
   int total_cnt;
   int total_ncnt;
 } data_head;
 
-// Initial Global struct that stores all relevent data.
+/**
+ * Initial Global struct that stores all relevent data.
+ **/
 data_head global_head;
+
+/**
+ * Declaration of function prototypes.
+ **/
 int proc_init(void);
 static void create_list(void);
 unsigned long virt2phys(struct mm_struct*, unsigned long);
+static int proc_open(struct inode *, struct file *);
 static int proc_show(struct seq_file *, void *);
 void print2log(void);
-static int proc_open(struct inode *, struct file *);
-void freeList(void);
 void proc_cleanup(void);
+void freeList(void);
 
-// proc_fops struct. proc_open must be defined and created before its calledfrom
+/**
+ * proc_fops struct. proc_open must be defined and created before its called.
+ **/
 static const struct file_operations proc_fops = {
   .owner = THIS_MODULE,
   .open = proc_open,
@@ -43,38 +55,43 @@ static const struct file_operations proc_fops = {
 };
 
 /**
- *  Called by module_init function to initial the kernal process.
- * 
- * Creates 
+ * Called by module_init function to initial the kernal process.
+ * Creates a list of all the processes and saves crutial data to it.
+ * Creates a report about the processes and outputs it to files in /proc/proc_report and /var/log/syslog.
  **/
-int proc_init () {
+int proc_init (void) {
   create_list();
   proc_create("proc_report", 0, NULL, &proc_fops);
   print2log();
   return 0;
 }
 
-static void create_list() {
+/**
+ * Generates a linked list of proc_data nodes, each with data of one proccess (with PID > 650)
+ * to be put in the report later.
+ **/
+static void create_list(void) {
   struct task_struct *task;
   struct vm_area_struct *vma;
   unsigned long prev, vpage;
   proc_data *proc_temp;
 
-  //
+  // Initializes a temporary node.
   proc_temp = kmalloc(sizeof(proc_data), GFP_KERNEL);
-  proc_temp->contig_pages = 0;
-  proc_temp->noncontig_pages = 0;
   proc_temp->next = NULL;
   
   // save the location of the first node of the list into the global variable.
   global_head.head = proc_temp;
   prev = 0;
-  vma = 0;
 
   for_each_process(task) {
     if(task->pid > 650) {
+      
       proc_temp->proc_id = task->pid;
       proc_temp->proc_name = task->comm;
+      proc_temp->contig_pages = 0;
+      proc_temp->noncontig_pages = 0;
+      
       vma = 0;
 
       if (task->mm && task->mm->mmap) {
@@ -92,15 +109,17 @@ static void create_list() {
       }
       proc_temp->next = kmalloc(sizeof(proc_data), GFP_KERNEL);
       proc_temp = proc_temp->next;
-      proc_temp->contig_pages = 0;
-      proc_temp->noncontig_pages = 0;
     }
   } 
+  // Clean the temporary pointer from memory.
   proc_temp->next = NULL;
   proc_temp = NULL;
   kfree(proc_temp);
 }
 
+/**
+ * 
+ **/
 unsigned long virt2phys(struct mm_struct *mm, unsigned long vpage) {
   pgd_t *pgd;
   p4d_t *p4d;
@@ -126,6 +145,13 @@ unsigned long virt2phys(struct mm_struct *mm, unsigned long vpage) {
 }
 
 /**
+ * creates, opens and writes to file in /proc/proc_report using the proc_show method.
+ **/
+static int proc_open(struct inode *inode, struct file *file) {
+  return single_open(file, proc_show, NULL);
+}
+
+/**
  * Using the gloabl_head variable, it prints the report of the data collected to /proc/proc_report.
  **/
 static int proc_show(struct seq_file *m, void *v) {
@@ -142,7 +168,7 @@ static int proc_show(struct seq_file *m, void *v) {
 /**
  * Using the gloabl_head variable, it prints the report of the data collected to /var/log/syslog files.
  **/
-void print2log() {
+void print2log(void) {
   proc_data *proc_temp = global_head.head;
   printk(KERN_INFO "PROCESS REPORT:\n");
   printk(KERN_INFO "proc_id,proc_name,contig_pages,noncontig_pages,total_pages\n");
@@ -153,23 +179,26 @@ void print2log() {
   printk(KERN_INFO "TOTALS,,%d,%d,%d\n", global_head.total_cnt, global_head.total_ncnt, global_head.total_cnt + global_head.total_ncnt);
 }
 
-static int proc_open(struct inode *inode, struct file *file) {
-  return single_open(file, proc_show, NULL);
+/**
+ * Called by module_init function to clean up and close the kernal process.
+ * Cleans up memory from allocated space.
+ **/
+void proc_cleanup(void) {
+  remove_proc_entry("proc_report", NULL);
+  freeList();
+  printk(KERN_INFO "procReport: performing cleanup of module\n");
 }
 
-void freeList() {
+/**
+ * Frees all the memory allocated to the proc_data nodes.
+ **/
+void freeList(void) {
   proc_data *proc_temp;  
   while(global_head.head) {
     proc_temp = global_head.head->next;
     kfree(global_head.head);
     global_head.head = proc_temp;
   }
-}
-
-void proc_cleanup(void) {
-  remove_proc_entry("proc_report", NULL);
-  freeList();
-  printk(KERN_INFO "procReport: performing cleanup of module\n");
 }
 
 MODULE_LICENSE("GPL");
